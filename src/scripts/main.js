788d0f0ba267c3529d8361f859c057a2c96deef9
Ian Teda <ian@teda.id.au>
2017-09-26 21:24:16 +1000

    Featured blog post index

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,25 +1,5 @@
 $(document).ready(function() {
-    
-    // MODAL
-    $('.modal').modal();
-
-    // DROPDOWNS
-    $('.dropdown-button').dropdown({
-        belowOrigin: true,
-    });
-    
-    // TABS
-    $('ul.tabs').tabs();
-
-    // SCROLLSPY
-    $('.scrollspy').scrollSpy();
-
     // SIDENAV
     $('.button-collapse').sideNav();
 
-    $('.carousel.carousel-slider').carousel({fullWidth: true});
-
-    $('.slider').slider();
-
-    $('.materialboxed').materialbox();
 });
