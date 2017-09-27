297d5e392dbdf76e8278cece291591fbfd636b13
Ian Teda <ian@teda.id.au>
2017-09-27 20:49:40 +1000

    Side Navigation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,18 @@
 $(document).ready(function() {
     // SIDENAV
-    $('.button-collapse').sideNav();
+    $('.button-collapse').sideNav({
+        // Default is 300
+        menuWidth: 250,
+        // Choose the horizontal origin
+        edge: 'right',
+        // Closes side-nav on <a> clicks, useful for Angular/Meteor
+        closeOnClick: true,
+        // Choose whether you can drag to open on touch screens,
+        draggable: true,
+        // A function to be called when sideNav is opened
+        onOpen: function(el) { },
+        // A function to be called when sideNav is closed
+        onClose: function(el) { },
+    });
 
 });
